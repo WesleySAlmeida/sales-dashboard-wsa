3369180aecadc8e78f499c0797e12539042c68a6
wesley <sw.almeida09@gmail.com>
2026-03-26 09:37:47 -0300

    Feat: Login page request

---
+++ src/utils/jwtExpirationDateConverter.ts
@@ -0,0 +1,11 @@
+/**
+ * Convert JWT exp in days
+ * @param exp - Number to be converted.
+ * @returns Converted exp in days.
+ */
+
+export function jwtExpirationDateConverter(exp: number): number {
+  const currentTime = Math.floor(Date.now() / 1000);
+  const secondsUntilExpiration = exp - currentTime;
+  const secondsInDay = 60 * 60 * 24;
+}
