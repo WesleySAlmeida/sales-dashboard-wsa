2141491570e3a7ae508948905c8cd69351514da8
wesley <sw.almeida09@gmail.com>
2026-04-28 11:14:17 -0300

    Feat: Create Profile Flow

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { use, useEffect } from 'react';
 import type { ChangeEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
@@ -28,8 +28,15 @@ import type {
   LoginPostData,
 } from '@/types';
 
+//REDUX
+import { useSelector } from 'react-redux';
+import type { RootState } from '@/redux';
+
 function Login() {
   const navigate = useNavigate();
+  const { email, message } = useSelector(
+    (state: RootState) => state.createProfile
+  );
   const inputs = [
     { type: 'email', placeholder: 'Email' },
     { type: 'password', placeholder: 'Senha' },
@@ -40,7 +47,7 @@ function Login() {
   const { formValues, formValid, handleChange } = useFormValidation(inputs);
 
   const handleMessage = (): MessageProps => {
-    if (!error) return { msg: '', type: 'success' };
+    if (!error) return { msg: message ?? '', type: 'success' };
     switch (error) {
       case 401:
         return { msg: 'Email e/ou senha inválido', type: 'error' };
@@ -70,6 +77,12 @@ function Login() {
     }
     if (Cookies.get('Authorization')) navigate('/home');
   }, [data, navigate]);
+
+  useEffect(() => {
+    if (email) {
+      handleChange(0, email);
+    }
+  }, [email]);
 
   return (
     <Box>
